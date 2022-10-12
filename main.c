0a0fcd5d42d427cb63b0d59ed0b5d93a84b4f346
muegan <2216869@sookmyung.ac.kr>
2022-10-12 23:12:12 +0900

    5번

--- main.c
+++ main.c
@@ -4,17 +4,50 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 
-void square(int a)
+int main(void)
 {
-	a=a*a;
-
+	int n,r;
+	
+	
+	return result;
 }
 
-int main(int argc, char *argv[]){
-	int a=2;
+int factorial()
+{
+	int res=1;
+	int n;
+	int i;
 	
-		square(a);
+	for(i=1; i<=n; i++)
+	{
+		res=res*i;
+		}
+	return res;
+}
+
+int combination(int n, int r)
+{
 	
-		printf("a=%i\n",a);
+	int a,b,c;
 	
+	a=factorial(n);
+	b=factorial(r);
+	c=factorial(n-r);
+	
+	return a/(b*c);
+}
+
+
+int get_integer(int n, int r, int res)
+{
+	printf("n을 입력하시오");
+	scanf("%i",&n);
+	
+	printf("r을 입력하시오");
+	scanf("%i",&r);
+	
+	res=combination(n,r);
+	printf("결과값은 %i입니다.",res);
+	
+	return 0;
 }
